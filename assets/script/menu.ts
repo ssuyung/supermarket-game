dc8d49467426ac47fe6cc0185aa9ac6fbc12e2b5
109062110 <ycchu8992@gmail.com>
2022-06-17 10:45:22 +0800

    menu

--- assets/script/menu.ts
+++ assets/script/menu.ts
@@ -10,19 +10,56 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class NewClass extends cc.Component {
 
-    @property(cc.Label)
-    label: cc.Label = null;
+    @property(cc.Toggle)
+    easy: cc.Toggle = null;
 
-    @property
-    text: string = 'hello';
+    @property(cc.Toggle)
+    medium: cc.Toggle = null;
 
-    // LIFE-CYCLE CALLBACKS:
+    @property(cc.Toggle)
+    hard: cc.Toggle = null;
 
-    // onLoad () {}
+    @property(cc.Toggle)
+    player1: cc.Toggle = null;
+
+    @property(cc.Toggle)
+    player2: cc.Toggle = null;
+
+    private mode=0;//easy: 0, medium: 1, hard: 2
+    private player=1;//number of players.
 
     start () {
-
+        let btn = new cc.Component.EventHandler();
+        btn.target = this.node;
+        btn.component = "menu";
+        btn.handler = "loadScene";
+        cc.find("Canvas/Start Button").getComponent(cc.Button).clickEvents.push(btn);
     }
 
-    // update (dt) {}
+    loadScene(){
+        this.controller();
+        console.log(this.mode, this.player);
+        if(this.mode==0 && this.player==1){
+            //cc.director.loadScene("Stage1 - 109062129");
+        }else if(this.mode==0 && this.player==2){
+            //cc.director.loadScene("Stage1 - 109062129");
+        }else if(this.mode==1 && this.player==1){
+            //cc.director.loadScene("Stage1 - 109062129");
+        }else if(this.mode==1 && this.player==2){
+            //cc.director.loadScene("Stage1 - 109062129");
+        }else if(this.mode==2 && this.player==1){
+            //cc.director.loadScene("Stage1 - 109062129");
+        }else if(this.mode==2 && this.player==2){
+            //cc.director.loadScene("Stage1 - 109062129");
+        }
+    }
+
+    controller(){
+        if(this.easy.isChecked) this.mode=0;
+        else if(this.medium.isChecked) this.mode=1;
+        else if(this.hard.isChecked) this.mode=2;
+
+        if(this.player1.isChecked) this.player=1;
+        else if(this.player2.isChecked) this.player=2;
+    }
 }
