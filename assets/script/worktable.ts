d77fb7d2b2c817b1861a64974f440ce8dd48bb04
unknown <ycchu8992@gmail.com>
2022-06-18 16:52:13 +0800

    worktable

--- assets/script/worktable.ts
+++ assets/script/worktable.ts
@@ -11,10 +11,47 @@ const {ccclass, property} = cc._decorator;
 export default class worktable extends cc.Component {
 
     private isworking = false;
+    private contagwithplayer = false;
+    private anim = null;
+    private flag_for_paused = 0;
+    //When animation isn't playing, there are 3 possible cases. 2 means stop, 1 means paused, 0 not play yet.
 
     start () {
-
+        this.anim = this.getComponentInChildren(cc.Animation)
+        console.log(this.anim.name);
     }
 
-    // update (dt) {}
+    onBeginContact (contact, self, other) {
+        if (other.node.name == "Player" && !this.contagwithplayer) {
+            this.contagwithplayer = true;
+        }
+    }
+
+    onEndContact(contact, self, other){
+        if (other.node.name == "Player" && this.contagwithplayer) {
+            this.contagwithplayer = false;
+            if(this.anim.getAnimationState("time_icon").isPlaying == false && this.flag_for_paused == 2){
+                this.isworking = false;
+                this.flag_for_paused = 0;
+            }
+        }
+    }
+
+    update (dt) {
+        if(this.isworking == true && this.contagwithplayer){
+            if(this.anim.getAnimationState("time_icon").isPaused){
+                this.anim.resume("time_icon");
+                this.flag_for_paused = 2;
+            }
+            else if(this.anim.getAnimationState("time_icon").isPlaying == false && this.flag_for_paused == 0){
+                this.anim.play("time_icon");
+                this.flag_for_paused = 2;
+            }
+        }else if(this.isworking == true && !this.contagwithplayer){
+            if(this.anim.getAnimationState("time_icon").isPlaying){
+                this.anim.pause("time_icon");
+                this.flag_for_paused = 1;
+            }
+        }
+    }
 }
