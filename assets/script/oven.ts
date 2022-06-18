a8504a1cd1fbb74591eb573efd823888340621cf
unknown <ycchu8992@gmail.com>
2022-06-18 23:51:05 +0800

    oven.ts and oven animation(stil not complete,but can use)

---
+++ assets/script/oven.ts
@@ -0,0 +1,43 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class oven extends cc.Component {
+
+    @property(cc.Prefab)
+    pizzaPrefab: cc.Prefab = null;
+
+    private player: cc.Node = null;
+
+    private isworking = false;
+    private contagwithplayer = false;
+    private anim = null;
+    private flag_for_paused = 0;
+    //When animation isn't playing, there are 3 possible cases. 1 means stop, 0 not play yet.
+
+    start () {
+        this.player = cc.find("Canvas/Player");
+        this.anim = this.getComponent(cc.Animation)
+    }
+
+    update (dt) {
+        if(this.anim.getAnimationState("oven").isPlaying == false && this.flag_for_paused == 1){
+            this.isworking = false;
+            var pizza = cc.instantiate(this.pizzaPrefab);
+            pizza.setPosition(cc.v2(this.node.x-1.5, this.node.y-5));
+            cc.find("Canvas").addChild(pizza);
+        }
+        if(this.isworking == true){
+            if(this.anim.getAnimationState("oven").isPlaying == false && this.flag_for_paused == 0){
+                this.anim.play("oven");
+                this.flag_for_paused = 1;
+            }
+        }
+    }
+}
