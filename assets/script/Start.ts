d29a0b252d9be1ea0922dd804127856a6c6702ff
109062129 <olgi9911@gmail.com>
2022-06-20 16:52:19 +0800

    Add Start scene

---
+++ assets/script/Start.ts
@@ -0,0 +1,29 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    @property(cc.Label)
+    label: cc.Label = null;
+
+    @property
+    text: string = 'hello';
+
+    onKeyDown(event){
+        if(event.keyCode == cc.macro.KEY.enter) {
+            cc.director.loadScene("menu");
+        }
+    }
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+    }
+
+    start () {
+        
+    }
+
+    // update (dt) {}
+}
